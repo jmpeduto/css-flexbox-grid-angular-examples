3fcdbc38754155076d28dd2d516f5e319fb45089
jmpeduto <jmpeduto@gmail.com>
2022-01-16 12:45:00 -0300

    create: add event change to two first radio flex-direction

--- src/app/components/codepen/codepen-grid-example/codepen-grid-example.component.ts
+++ src/app/components/codepen/codepen-grid-example/codepen-grid-example.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-codepen-grid-example',
   templateUrl: './codepen-grid-example.component.html',
-  styleUrls: ['./codepen-grid-example.component.scss']
+  styleUrls: ['./codepen-grid-example.component.scss'],
 })
 export class CodepenGridExampleComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // this.changeFlex('flex-direction', 'direction');
   }
 
+  changeFlex(input:any, inputClass: any, itemIdSelector: any) {
+    document
+      .querySelector('#' + itemIdSelector)
+      ?.setAttribute('class', inputClass + ' ' + input.value);
+  }
 }
