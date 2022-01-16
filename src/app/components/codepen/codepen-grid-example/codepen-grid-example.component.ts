852d7f14790b1323cbff50192029e55864e81cd3
jmpeduto <jmpeduto@gmail.com>
2022-01-16 17:52:27 -0300

    update: funcionando necesita refactor de todas las funciones con variables estaticas

--- src/app/components/codepen/codepen-grid-example/codepen-grid-example.component.ts
+++ src/app/components/codepen/codepen-grid-example/codepen-grid-example.component.ts
@@ -12,16 +12,58 @@ export class CodepenGridExampleComponent implements OnInit {
     // this.changeFlex('flex-direction', 'direction');
   }
 
-  changeFlex(input: any, targetClass: any, itemIdSelector: any) {
+  changeFlex(input: any, containerClass: any, itemIdSelector: any) {
     document
       .querySelector('#' + itemIdSelector)
-      ?.setAttribute('class', targetClass + ' ' + input.value);
+      ?.setAttribute('class', containerClass + ' ' + input.value);
   }
 
-  changeItemFlex(input: any, targetClass: any, itemIdSelector: any) {
-    console.log('class', targetClass + ' ' + input.value);
+  changeItemFlex(input: any, containerClass: any, itemIdSelector: any) {
+    console.log('class', containerClass + ' ' + input.value);
     document
-    .querySelector('#' + itemIdSelector)
-    ?.setAttribute('class', targetClass + ' ' + input.value);
+      .querySelector('#' + itemIdSelector)
+      ?.setAttribute('class', containerClass + ' ' + input.value);
+  }
+
+  changeFlexGrow(input: any, itemIdSelector: any) {
+    let n = isNaN(input.value) ? 0 : input.value;
+    console.log('Grow: ' + n);
+    let element: any = document.querySelector('#' + itemIdSelector);
+    element.style.WebkitFlexGrow = n;
+    element.style.flexGrow = n;
+  }
+
+  changeFlexShrink(input: any, itemIdSelector: any) {
+    let n = isNaN(input.value) ? 0 : input.value;
+    console.log('Shrink: ' + n);
+    let element: any = document.querySelector('#' + itemIdSelector);
+    element.style.WebkitFlexShrink = n;
+    element.style.flexShrink = n;
+  }
+
+  changeFlexBasis(input: any, itemIdSelector: any) {
+    let n = isNaN(input.value) ? 0 : input.value;
+    console.log('Basis: ' + n);
+    let element: any = document.querySelector('#' + itemIdSelector);
+    element.style.WebkitFlexBasis = n + 'px';
+    element.style.flexBasis = n + 'px';
+  }
+
+  changeFlexOrder(input: any, itemIdSelector: any) {
+    let n = isNaN(input.value) ? 0 : input.value;
+    console.log('Order: ' + n);
+    let element: any = document.querySelector('#' + itemIdSelector);
+    element.style.WebkitOrder = n;
+    element.style.order = n;
+  }
+
+  changeAll(input1Id:any, input2Id:any, input3Id:any, item:any) {
+    console.log(input1Id + " " + input2Id + " " + input3Id + " " + item);
+    let input1 = document.getElementById(input1Id);
+    let input2 = document.getElementById(input2Id);
+    let input3 = document.getElementById(input3Id);
+    this.changeFlexBasis(input1, item);
+    this.changeFlexGrow(input2, item);
+    this.changeFlexShrink(input3, item);
   }
 }
